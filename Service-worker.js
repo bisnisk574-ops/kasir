a02834e7e44dfbdc7a815572cec4a2ce8cd01392
Gokong03 <118730122+Gokong03@users.noreply.github.com>
2025-09-09 01:21:59 +0800

    Add files via upload

---
+++ Service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "kasir-sgcell-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo.png",
+  "/app.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
